a4b18d191879bcdf1244070484ff65da5335fbef
nurnaby <sohag.biit@gmail.com>
2023-09-27 20:48:02 +0600

    task 5

--- assignment_3/task_5.php
+++ assignment_3/task_5.php
@@ -1,6 +1,26 @@
 <?php
 
-// Create a PHP function called generatePassword($length) that generates a random password of the specified length. The password should include lowercase letters, uppercase letters, numbers, and special characters (!@#$%^&*()_+). Write a PHP program to generate a password with a length of 12 characters using this function and print the password.
+function generatePassword($length) {
+    // Define character sets for lowercase, uppercase, numbers, and special characters
+    $lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    $uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numberChars = '0123456789';
+    $specialChars = '!@#$%^&*()_+';
 
+    $allChars = $lowercaseChars . $uppercaseChars . $numberChars . $specialChars;
 
+    $password = '';
+
+    // Generate the password
+    for ($i = 0; $i < $length; $i++) {
+        $randomChar = $allChars[rand(0, strlen($allChars) - 1)];
+        $password .= $randomChar;
+    }
+
+    return $password;
+}
+
+$password = generatePassword(12);
+
+echo "Generated Password: $password";
 ?>
